eda1bd997755bbc1e1970f45a1d2aa3f4ca70b73
Michael Rockhold <1175404+michaelrockhold@users.noreply.github.com>
2024-04-15 17:25:05 -0700

    use a real API for greater artistic versimilitude

--- demotool/main.swift
+++ demotool/main.swift
@@ -6,6 +6,30 @@
 //
 
 import Foundation
+import Speech
+
+func getAuthorization() async throws -> SFSpeechRecognizerAuthorizationStatus {
+    return await withCheckedContinuation(function: "getAuthorization") { continuation in
+        SFSpeechRecognizer.requestAuthorization { status in
+            continuation.resume(with: .success(status)) // always "succeeds", as Error type is Never
+        }
+    }
+}
 
 print("Hello, World!")
 
+do {
+    let r = try await getAuthorization()
+
+    switch r {
+    case .authorized:
+        print("Speech Recognition is authorized.")
+
+    default:
+        print("Speech Recognition is not authorized: \(r)")
+    }
+} catch {
+    print("Error attempting to authorize Speech Recognition: \(error)")
+}
+
+print("All done")
